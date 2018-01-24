8c11cca27323817a4cb12e2c1a23e1bff65255a5
LorenzaIacobuzio <lorenza.iacobuzio@gmail.com>
2018-01-24 15:36:16 +0000

    update

--- Analyzers/include/HeavyNeutrinoMassScan.hh
+++ Analyzers/include/HeavyNeutrinoMassScan.hh
@@ -39,108 +39,7 @@ public:
   void PostProcess() {}
   void DrawPlot() {}
 
-  Double_t ComputeHNLMass(KinePart*);
-  Double_t ComputeL(TVector3, TVector3, TVector3);
-  Double_t ComputeNDecayProb(KinePart*, Double_t, Double_t);
-  Double_t ComputeNReachProb(KinePart*, Double_t, Double_t);
-  Double_t PhaseSpace(Double_t, Double_t, Double_t);
-  Double_t PhaseSpaceFactor(Double_t, Double_t, Double_t);
-  Double_t TwoBodyBR(Double_t, Double_t, Double_t, Int_t, Bool_t);
-  Double_t ThreeBodyBR(Double_t, Double_t, Double_t, Double_t, Int_t, Bool_t);
-  std::string ThreeBodyFunction(Double_t, Double_t);
-  Double_t Gamma2(Double_t, Double_t, Double_t, Double_t, Bool_t);                          
-  Double_t GammaLeptonNu3(Double_t, Double_t, Double_t, Bool_t);                      
-  Double_t GammaTot(Double_t);                                               
-  Double_t tauN(Double_t);                                                                             
-  Double_t lambda(Double_t, Double_t, Double_t);
-  Double_t ComputeProd(KinePart*, Double_t);
-  Double_t ComputeDecay(Double_t);
-
 protected:
-
-  // Histos
-
-  TH2D *fhReach;
-  TH2D *fhDecay;
-  TH2D *fhWeight;
-
-  TGraph *fgAcc;
-  TGraph *fgYield;
-  TGraph *fgGammaTot;
-  TGraph *fgTau;
-
-  // Masses                                                                                        
-
-  Double_t fMe;
-  Double_t fMmu;
-  Double_t fMtau;
-  Double_t fMpi;
-  Double_t fMpi0;
-  Double_t fMrho;
-  Double_t fMrho0;
-  Double_t fMeta;
-  Double_t fMetaprime;
-  Double_t fMD;
-  Double_t fMDS;
-  Double_t fMD0;
-  Double_t fMK;
-  Double_t fMK0;
-  Double_t fMp;
-  Double_t fMKStar;
-  Double_t fMK0Star;
-
-  // Lifetimes                                                                                          
-
-  Double_t fDlife;
-  Double_t fDSlife;
-  Double_t fD0life;
-  Double_t ftaulife;
-
-  // Constants                                                                                          
-
-  Double_t fhc;
-  Double_t fcLight;
-  Double_t fGF;
-  Double_t fPi;
-  Double_t fRho;
-  Double_t fD;
-  Double_t fDS;
-  Double_t fK;
-  Double_t fEta;
-  Double_t fEtaprime;
-  Double_t fsigmacc;
-
-  // CKM                                                                                              
-
-  Double_t fVcs;
-  Double_t fVcd;
-  Double_t fVud;
-  Double_t fVus;
-
-  // Form factors, pseudoscalar and vector mesons                                                       
-
-  Double_t fDK0;
-  Double_t fDpi0;
-  Double_t fD0K;
-  Double_t fD0pi;
-  Double_t fgDK0;
-  Double_t fgDpi0;
-  Double_t fgD0K;
-  Double_t fgD0pi;
-  Double_t fA0D;
-  Double_t fA1D;
-  Double_t fA2D;
-  Double_t fVD;
-  Double_t fA0D0;
-  Double_t fA1D0;
-  Double_t fA2D0;
-  Double_t fVD0;
-
-  // Fragmentation fractions                                                                            
-
-  Double_t ffD;
-  Double_t ffD0;
-  Double_t ffDS;
 
   // NA62 parameters                                                                                    
 
@@ -164,21 +63,18 @@ protected:
   Double_t fzCHODPlane;
   Double_t frMinCHOD;
   Double_t frMaxCHOD;
-  Double_t* fzStraw;
-  Double_t* fxStrawChamberCentre;
+  Double_t fzStraw[4];
+  Double_t fxStrawChamberCentre[4];
 
   // Other parameters                                                                                   
 
   Double_t fDBeProdProb;
   Double_t fDCuProdProb;
   Double_t fDDecayProb;
-  Double_t fUSquared;
-  Double_t fUeSquared;
-  Double_t fUmuSquared;
-  Double_t fUtauSquared;
-  Double_t fUeSquaredRatio;
-  Double_t fUmuSquaredRatio;
-  Double_t fUtauSquaredRatio;
+  TwoLinesCDA *fCDAcomp;
+  PointLineDistance *fDistcomp;
+  LAVMatching *fLAVMatching;
+  SAVMatching *fSAVMatching;
 
   // Scan variables
 
@@ -200,12 +96,16 @@ protected:
   Double_t fYield[fN];
   Double_t fMasses[fN];
 
-  // Other variables
+  // Histos
 
-  TwoLinesCDA *fCDAcomp;
-  PointLineDistance *fDistcomp;
-  LAVMatching *fLAVMatching;
-  SAVMatching *fSAVMatching;
+  TH2D *fhReach;
+  TH2D *fhDecay;
+  TH2D *fhWeight;
+
+  TGraph *fgAcc;
+  TGraph *fgYield;
+  TGraph *fgGammaTot;
+  TGraph *fgTau;
 };
 
 #endif
