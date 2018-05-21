13e2a39abf60595307113c862f8b4e3294f7e8b2
Francesco Gonnella <gonnella@macgonnella.dyndns.cern.ch>
2018-05-21 18:03:05 +0200

    update

--- MyScripts/Others/AnalyzerPlots/AllModels.sh
+++ MyScripts/Others/AnalyzerPlots/AllModels.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-#scp liacobuz@lxplus.cern.ch:~/NA62AnalysisTool/bla*.root liacobuz@lxplus.cern.ch:~/NA62AnalysisTool/minc*.root ~/Desktop
-cd ~/Desktop/GitVarious/MyScripts/Others/AnalyzerPlots/
-#root -b -q 'Plots.C("~/Desktop/MCnote/images/Plots/", "~/Desktop/bla.root", "~/Desktop/minc.root")'
-root -b -q 'Plots.C("~/Desktop/MCnote/images/Plots/", "~/Desktop/bla1.root", "~/Desktop/minc1.root")'
-root -b -q 'Plots.C("~/Desktop/MCnote/images/Plots/", "~/Desktop/bla2.root", "~/Desktop/minc2.root")'
-root -b -q 'Plots.C("~/Desktop/MCnote/images/Plots/", "~/Desktop/bla3.root", "~/Desktop/minc3.root")'
+cd ~/NA62AnalysisTool
+source ~/NA62AnalysisTool/scripts/env.sh
+./bin-slc6/MyAnalyzer -i /afs/cern.ch/work/l/liacobuz/private/ImportantHistos/Reco/Reco.root -o $11.root -p "HeavyNeutrinoScan:UeSquaredRatio=52;UmuSquaredRatio=1;UtauSquaredRatio=1&HeavyNeutrino:UeSquaredRatio=52;UmuSquaredRatio=1;UtauSquaredRatio=1"
+./bin-slc6/MyAnalyzer -i $11.root -o $21.root --histo
+./bin-slc6/MyAnalyzer -i /afs/cern.ch/work/l/liacobuz/private/ImportantHistos/Reco/Reco.root -o $12.root -p "HeavyNeutrinoScan:UeSquaredRatio=1;UmuSquaredRatio=16;UtauSquaredRatio=3.8&HeavyNeutrino:UeSquaredRatio=1;UmuSquaredRatio=16;UtauSquaredRatio=3.8"
+./bin-slc6/MyAnalyzer -i $12.root -o $22.root --histo
+./bin-slc6/MyAnalyzer -i /afs/cern.ch/work/l/liacobuz/private/ImportantHistos/Reco/Reco.root -o $13.root -p "HeavyNeutrinoScan:UeSquaredRatio=0.061;UmuSquaredRatio=1;UtauSquaredRatio=4.3&HeavyNeutrino:UeSquaredRatio=0.061;UmuSquaredRatio=1;UtauSquaredRatio=4.3"
+./bin-slc6/MyAnalyzer -i $13.root -o $23.root --histo
