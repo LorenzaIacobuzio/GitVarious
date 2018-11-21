ec69ffba891db1604ddb4a45d73239f06c3b4c59
LorenzaIacobuzio <lorenza.iacobuzio@gmail.com>
2018-11-21 16:21:20 +0000

    update

--- Update.sh
+++ Update.sh
@@ -4,6 +4,7 @@ scp -r $LX/Analyzers .
 scp $LX/Jobs/* MyScripts/Jobs
 scp $LX/HaddScript/copyFiles.C MyScripts/Others/HaddScript
 scp $LX/HaddScript/Hadd.py MyScripts/Others/HaddScript
+scp $LX/HaddScript/Hadd.sh MyScripts/Others/HaddScript
 git status
 git add -A
 git commit -m "update"
