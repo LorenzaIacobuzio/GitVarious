4c88932ac1a1869b9a03b1deffe2a2c8404ea12a
Lorenza <lorenza@maclorenza.lan>
2019-02-27 15:09:46 +0000

    update

--- MyScripts/Others/HaddScript/DataHadd/DataHadd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source /afs/cern.ch/user/l/liacobuz/na62fw/NA62Reconstruction/scripts/env.sh
-source /afs/cern.ch/user/l/liacobuz/NA62AnalysisTool/scripts/env.sh
-cp /afs/cern.ch/user/l/liacobuz/NA62AnalysisTool/HaddScript/Hadd.py .
-echo $1 $2 $3
-./Hadd.py $1 $2 $3
--- MyScripts/Others/HaddScript/MCHadd/SplitIntoSubfolders.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd $1
-
-i=1;while read l;do mkdir $i;mv $l $((i++));done< <(ls|xargs -n100)
-
