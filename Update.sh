c415d1d878712b653667f3a9df4aeed81d967475
LorenzaIacobuzio <lorenza.iacobuzio@gmail.com>
2019-08-26 15:25:48 +0200

    update

--- Update.sh
+++ Update.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 scp -r liacobuz@lxplus.cern.ch:/afs/cern.ch/user/l/liacobuz/NA62AnalysisTool/Analyzers .
-scp -r liacobuz@lxplus.cern.ch:/afs/cern.ch/user/l/liacobuz/NA62AnalysisTool/HaddScript MyScripts/Others/
 scp liacobuz@lxplus.cern.ch:/afs/cern.ch/user/l/liacobuz/NA62AnalysisTool/CondorJobs/* MyScripts/Jobs/
 git status
 git add -A
